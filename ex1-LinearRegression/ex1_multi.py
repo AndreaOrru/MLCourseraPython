9dfc277dcae579335d964a256a9195b735fb72ae
Andrea Orru <andreaorru1991@gmail.com>
2016-10-12 01:55:51 -0400

    Normal equation and SKLearn methods.

--- ex1-LinearRegression/ex1_multi.py
+++ ex1-LinearRegression/ex1_multi.py
@@ -2,9 +2,14 @@
 
 from matplotlib import pyplot as plt
 import numpy as np
+import sklearn.linear_model
 
 from feature_normalize import feature_normalize
 from gradient_descent_multi import gradient_descent_multi
+
+# Ignore errors about non-present drivers on OS X:
+import warnings
+warnings.filterwarnings(action='ignore', module='scipy', message='^internal gelsd')
 
 
 #############################
@@ -16,11 +21,13 @@ X = data[:, :2]  # Extract the first two columns (features).
 y = data[:,  2]  # Extract the last column (target values).
 m = y.size       # Number of training samples.
 
+X_orig = X       # Save the original matrix.
+
 
 #############################
 #   FEATURE NORMALIZATION   #
 #############################
-print('Normalizing features...')
+print('NORMALIZING FEATURES...')
 X, mu, sigma = feature_normalize(X)
 print('  mu:     {}'.format(mu))
 print('  sigma:  {}'.format(sigma))
@@ -36,7 +43,7 @@ X = np.c_[np.ones((m, 1)), X]  # Columns of 1 for linear algebra purposes.
 #############################
 #      GRADIENT DESCENT     #
 #############################
-print('Running gradient descent...')
+print('RUNNING GRADIENT DESCENT...')
 
 # Parameters:
 alpha = 0.01
@@ -46,13 +53,12 @@ iterations = 1500
 theta = np.zeros(3)
 theta, J_history = gradient_descent_multi(X, y, theta, alpha, iterations)
 print('  Theta after gradient descent: {}'.format(theta))
-print()
 
 
 #############################
 #      TEST PREDICTION      #
 #############################
-area = 1650
+area = 2000
 rooms = 3
 
 # Normalize and augment features with x0 = 1:
@@ -61,8 +67,43 @@ features = np.c_[[1], features]
 
 price_prediction = features.dot(theta)[0]
 
-print('Predicted price for a house with {} sq ft '
+print('  Predicted price for a house with {} sq ft '
       'and {} rooms: ${:.2f}'.format(area, rooms, price_prediction))
+print()
+
+
+#############################
+#      NORMAL EQUATIONS     #
+#############################
+print('RUNNING NORMAL EQUATIONS SOLVER...')
+
+# X is our matrix of features augmented with x0 = 1:
+norm_eq_theta = np.linalg.pinv((X.T).dot(X)).dot(X.T).dot(y)
+print('  Theta after normal equations: {}'.format(norm_eq_theta))
+
+# Predict price using normal equations:
+norm_eq_price = X.dot(norm_eq_theta)[0]
+
+print('  Predicted price (normal equation method) for a house with {} sq ft '
+      'and {} rooms: ${:.2f}'.format(area, rooms, norm_eq_price))
+print()
+
+
+#############################
+#    SKLEARN LINEAR MODEL   #
+#############################
+print('RUNNING SKLEARN LINEAR REGRESSION...')
+
+# X_orig is our matrix of features WITHOUT the augmented x0 = 1 column:
+sk_linear_regression = sklearn.linear_model.LinearRegression()
+sk_linear_regression.fit(X_orig, y)
+
+predictions = sk_linear_regression.predict([[area, rooms]])
+sklearn_price = predictions[0]
+
+print('  Predicted price (SKLearn linear regression) for a house with {} sq ft '
+      'and {} rooms: ${:.2f}'.format(area, rooms, sklearn_price))
+print()
 
 
 #############################
